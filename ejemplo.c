11028e95c682433b7082011e462cd751d8ab2d89
Ángela_Aquise <aaquises@unsa.edu.pe>
2024-10-05 19:33:29 -0500

    Solucionamos el error de violacion de segmento y logramos dibujar una linea horizontal

--- ejemplo.c
+++ ejemplo.c
@@ -6,9 +6,9 @@ void display(){
 
 //Guardamos las imagen en un puntero a puntero para utlizarlo mas adelante
 // Piezas blancas
-  char** whiteKing = king;
-  char** whiteQueen = queen;
-  char** whiteRook = rook;
+//  char** whiteKing = king;
+//  char** whiteQueen = queen;
+//  char** whiteRook = rook;
   char** whiteBishop = bishop;
   char** whiteKnight = knight;
   char** whitePawn = pawn;
@@ -22,15 +22,16 @@ void display(){
   char** blackPawn = reverse(pawn);                          //
 ////////////////////////
 ///
-    char** whiteSquare = whiteSquare; a
+    char** white = repeatH(whiteSquare, 7); 
 
-    char** firstRow = join(whiteKing, repeatH(whiteSquare, 7));
+//    char** firstRow = join(whiteKing, repeatH(whiteSquare, 7));
 
-    char** otherRow = repeatH(whiteSquare, 8);
+//    char** otherRow = repeatH(whiteSquare, 8);
 
-    char** chessBoard = up(firstRow, repeatV(otherRow, 7));
+//    char** chessBoard = up(firstRow, repeatV(otherRow, 7));
 
-    interpreter(chessBoard);
+//    interpreter(chessBoard);
+    interpreter(white);
 
 
 //liberamos la memoria ya que reverse usa malloc
