f29b59bb4fe57ff9145b07665fa6068fe3809766
Ángela_Aquise <aaquises@unsa.edu.pe>
2024-10-05 20:05:24 -0500

    Logramos crear la figura del ejercicio 2

---
+++ pro02.c
@@ -0,0 +1,11 @@
+#include "chess.h"
+#include "figures.h"
+#include <stdlib.h>
+#include <stdio.h>
+void display(){
+  char** white = whiteSquare;
+  char** black = reverse(whiteSquare);
+ // char** whiteBlack = join(white,black);
+  char** firstRow = repeatH(join(black,white),4);
+  interpreter(firstRow);
+}
