b71a99d5185b2dcee2e9e6095e0f8eb17ce977ba
Developer <developerys@gmail.com>
2024-10-07 10:19:58 -0500

    Posicionamos todas piezas en el tablero usando nuestro arreglo bidimensional

--- ejemplo.c
+++ ejemplo.c
@@ -4,19 +4,19 @@
 #include <stdio.h>
 void display(){
 ////PIEZAS BLANCAS
-  char** whiteKing = king;
+  char** whiteKing = flipH(king);
   char** whiteQueen = queen;
   char** whiteRook = rook;
   char** whiteBishop = bishop;
   char** whiteKnight = knight;
   char** whitePawn = pawn;
   //// PIEZAS NEGRAS
-  char** blackKing = reverse(king);
-  char** blackQueen = reverse(queen);
-  char** blackRook = reverse(rook);
-  char** blackBishop = reverse(bishop);
-  char** blackKnight = reverse(knight);
-  char** blackPawn = reverse(pawn);
+  char** blackKing = flipH(reverse(king));
+  char** blackQueen = flipH(reverse(queen));
+  char** blackRook = flipH(reverse(rook));
+  char** blackBishop = flipH(reverse(bishop));
+  char** blackKnight = flipH(reverse(knight));
+  char** blackPawn = flipH(reverse(pawn));
  //// ARREGLO BIDIMENSIONAL
   char** tablero[8][8];
 
@@ -45,19 +45,31 @@ void display(){
     }
   }
 
-  tablero[0][0] = superImpose(rook,tablero[0][0]);      
-  tablero[0][1] = superImpose(knight,tablero[0][1]);    
+  tablero[0][0] = superImpose(whiteRook,tablero[0][0]);      
+  tablero[0][1] = superImpose(whiteKnight,tablero[0][1]);    
   tablero[0][2] = superImpose(bishop,tablero[0][2]);   
   tablero[0][3] = superImpose(queen,tablero[0][3]);    
-  tablero[0][4] = superImpose(king,tablero[0][4]);      
+  tablero[0][4] = superImpose(whiteKing,tablero[0][4]);      
   tablero[0][5] = superImpose(bishop,tablero[0][5]);    
   tablero[0][6] = superImpose(knight,tablero[0][6]);  
   tablero[0][7] = superImpose(rook,tablero[0][7]);
 
+  tablero[7][0] = superImpose(blackRook,tablero[7][0]);      
+  tablero[7][1] = superImpose(blackKnight,tablero[7][1]);    
+  tablero[7][2] = superImpose(blackBishop,tablero[7][2]);   
+  tablero[7][3] = superImpose(blackQueen,tablero[7][3]);    
+  tablero[7][4] = superImpose(blackKing,tablero[7][4]);      
+  tablero[7][5] = superImpose(blackBishop,tablero[7][5]);    
+  tablero[7][6] = superImpose(blackKnight,tablero[7][6]);  
+  tablero[7][7] = superImpose(blackRook,tablero[7][7]);
+  
   for (int j = 0; j < 8; j++) {
     tablero[1][j] = superImpose(pawn,tablero[1][j]);  
   }
 
+  for (int j = 0; j < 8; j++) {
+    tablero[6][j] = superImpose(blackPawn,tablero[6][j]);  
+  }
   char** filaCompleta[8];
   for (int i = 0; i < 8; i++) {
     filaCompleta[i] = tablero[i][0];
