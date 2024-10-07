c58e89c069ec26c6a37e8dc2543982f0924de1f3
Developer <developerys@gmail.com>
2024-10-07 09:55:20 -0500

    se logro dibugar la fichas blanzas superponiendolas

--- ejemplo.c
+++ ejemplo.c
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 void display(){
-
-
+////PIEZAS BLANCAS
+  char** whiteKing = king;
+  char** whiteQueen = queen;
+  char** whiteRook = rook;
+  char** whiteBishop = bishop;
+  char** whiteKnight = knight;
+  char** whitePawn = pawn;
+  //// PIEZAS NEGRAS
+  char** blackKing = reverse(king);
+  char** blackQueen = reverse(queen);
+  char** blackRook = reverse(rook);
+  char** blackBishop = reverse(bishop);
+  char** blackKnight = reverse(knight);
+  char** blackPawn = reverse(pawn);
  //// ARREGLO BIDIMENSIONAL
   char** tablero[8][8];
 
@@ -33,16 +45,17 @@ void display(){
     }
   }
 
-////CREAMOS EL TABLERO B/N
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      if ((i + j) % 2 == 0) {
-        tablero[i][j] = whiteSquare;  
-      } 
-      else {
-        tablero[i][j] = reverse(whiteSquare);  
-      }
-    }
+  tablero[0][0] = superImpose(rook,tablero[0][0]);      
+  tablero[0][1] = superImpose(knight,tablero[0][1]);    
+  tablero[0][2] = superImpose(bishop,tablero[0][2]);   
+  tablero[0][3] = superImpose(queen,tablero[0][3]);    
+  tablero[0][4] = superImpose(king,tablero[0][4]);      
+  tablero[0][5] = superImpose(bishop,tablero[0][5]);    
+  tablero[0][6] = superImpose(knight,tablero[0][6]);  
+  tablero[0][7] = superImpose(rook,tablero[0][7]);
+
+  for (int j = 0; j < 8; j++) {
+    tablero[1][j] = superImpose(pawn,tablero[1][j]);  
   }
 
   char** filaCompleta[8];
